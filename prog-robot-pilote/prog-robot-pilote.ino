72035004fbdda0013fac063acbe83b2e5e86a3b7
Benjamin Mouëza <70459262+Moueben99@users.noreply.github.com>
2023-04-25 17:00:28 -0400

    prog pas à pas et déclaration des broches #2

---
+++ prog-robot-pilote/prog-robot-pilote.ino
@@ -0,0 +1,17 @@
+#include <Servo.h> // Utilisation de la bibliothèque Servo.h
+#define pinServo 9 // Broche pour le signal du servomoteur X
+const int SW_pin = 2; // Pin digital pour le bouton du joystick
+const int X_pin = 0; // Pin analogique pour la coordonnée X
+const int Y_pin = 1; // Pin analogique pour la coordonnée Y
+unsigned short X; // Variables pour les valeurs du joystick
+unsigned short Y; // Variables pour les valeurs du joystick
+Servo Sx; // Déclaration du Servo
+
+void setup() {
+
+}
+
+
+void loop() {
+
+}
